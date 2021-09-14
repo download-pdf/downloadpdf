65e80ff2e2bc67eea15e508f7f89f4f6267861cf
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-09-14 23:57:44 +0545

    Add CLI app main.py is the main CLI application that calls downloadPDF

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import argparse
+from downloadpdf import downloadPDF
+
+parser = argparse.ArgumentParser(
+    description='Download PDFs from URL.')
+parser.add_argument('url', metavar='url', 
+    type=str, help='URL')
+args = parser.parse_args()
+downloadPDF(args.url)
