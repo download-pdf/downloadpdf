ad3eb211256145c696ebbbe825d72fbd35c57dfd
Anit Shrestha Manandhar <codeanit@gmail.com>
2021-09-24 17:44:14 +0545

    Delete diff file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='downloadpdf',
-      version='1.0.5',
+      version='1.0.6',
       description='Download PDF from URL',
       long_description='For more information visit code repository.',
       url='https://github.com/PythonCheatsheet/downloadpdf/',
